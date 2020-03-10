917da18e8b0e79110c3f2798d7ea6397a9202f7b
zodgevaibhav <zodgevaibhav@gmail.com>
2020-03-10 12:50:16 -0400

    network scan with disctionary print

--- security-test-programs/network_scanning/network_scan_cstm_list_dictionary.py
+++ security-test-programs/network_scanning/network_scan_cstm_list_dictionary.py
@@ -9,7 +9,12 @@ def scan(ip):
     print("--------------------------------------------------------------------")
     print("  IP  \t\t\t\t MAC  ")
     print("--------------------------------------------------------------------")
+
+    machine_list=[]
     for element in answers:
+        #print(element[0].show())
+        #print(element[1].show())
+        machine_list.append({"ip": element[1].psrc, "mac": element[1].hwsrc})
         print(element[1].psrc+"\t\t\t"+element[1].hwsrc)
 
 
@@ -18,5 +23,11 @@ def scan(ip):
     #print(arp_request_broadcast.show())
     #scapy.ls(scapy.ARP())
     #scapy.ls(scapy.Ether())
+
+    print("\n\n------------------- Dictionary Print -----------------------------------\n")
+
+    print(machine_list[0].get("ip")+" - "+machine_list[0].get("mac"))
+    print(machine_list[1].get("ip")+" - "+machine_list[1].get("mac"))
+    print(machine_list[2].get("ip")+" - "+machine_list[2].get("mac"))
 
 scan("10.0.2.1/24")
