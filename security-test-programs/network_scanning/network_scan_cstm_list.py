27a4638926dede1c2201105101260b1ae161e8f3
zodgevaibhav <zodgevaibhav@gmail.com>
2020-03-10 11:58:08 -0400

    network scan with list print

--- security-test-programs/network_scanning/network_scan_cstm_list.py
+++ security-test-programs/network_scanning/network_scan_cstm_list.py
@@ -5,15 +5,18 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)  # set destination ip or ip range
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")  # set desetination mac as broadcast mac
     arp_request_broadcast = broadcast/arp_request
-    answered, unanswered = scapy.srp(arp_request_broadcast,timeout=1)
-    print(answered.summary())
-    print(unanswered.summary())
+    answers, unanswered = scapy.srp(arp_request_broadcast,timeout=1, verbose=False)
+    print("--------------------------------------------------------------------")
+    print("  IP  \t\t\t\t MAC  ")
+    print("--------------------------------------------------------------------")
+    for element in answers:
+        print(element[1].psrc+"\t\t\t"+element[1].hwsrc)
+
 
     #print(arp_request.summary())
     #print(broadcast.summary())
     #print(arp_request_broadcast.show())
     #scapy.ls(scapy.ARP())
     #scapy.ls(scapy.Ether())
-
 
 scan("10.0.2.1/24")
